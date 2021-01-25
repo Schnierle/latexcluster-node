8926d8ea58904ce21557345cab27169bfda2919c
Marc Schnierle <36483773+Schnierle@users.noreply.github.com>
2021-01-25 22:13:28 +0100

    Delete l4ttimezone.sh

--- l4ttimezone.sh
+++
@@ -1,6 +0,0 @@
-# set noninteractive installation
-export DEBIAN_FRONTEND=noninteractive
-
-# set your timezone
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
